7b389c2dc437a8f4510ffef7eceeef2a7869749f
Khubaib Zubairi <Khubaibzubairi@outlook.com>
2022-11-21 19:46:37 +0500

    dynamic frontend

--- src/app/pages/home/components/section-popular/components/all-posts/all-posts.component.ts
+++ src/app/pages/home/components/section-popular/components/all-posts/all-posts.component.ts
@@ -1,15 +1,74 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PostSchema } from 'src/app/pages/Posts/post.schema';
+import { PostService } from 'src/app/pages/Posts/post.service';
 
 @Component({
   selector: 'app-all-posts',
   templateUrl: './all-posts.component.html',
-  styleUrls: ['./all-posts.component.scss']
+  styleUrls: ['./all-posts.component.scss'],
 })
 export class AllPostsComponent implements OnInit {
+  constructor(
+    private postService: PostService,
+    private readonly route: Router
+  ) {}
 
-  constructor() { }
+  confirmationState: boolean = true;
+  allPosts!: PostSchema[];
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    await this.getAllPosts();
+    setTimeout(() => {
+      for (let i = 0; i < this.allPosts.length; i++) {
+        const dots = document.querySelector(
+          `.all-posts-dots${i}`
+        ) as HTMLElement;
+
+        const options = document.querySelector(
+          `.all-posts-options${i}`
+        ) as HTMLElement;
+
+        const deleteOptions = document.querySelector(
+          `.all-posts-delete${i}`
+        ) as HTMLElement;
+
+        const deleteConfirmation = document.querySelector(
+          `.all-posts-confirmation${i}`
+        ) as HTMLElement;
+
+        const No = document.querySelector(`.all-posts-No${i}`) as HTMLElement;
+
+        dots?.addEventListener('click', () => {
+          options!.classList.toggle('hidden');
+          console.log(dots);
+        });
+
+        deleteOptions.addEventListener('click', () => {
+          deleteConfirmation.style.display = 'flex';
+        });
+
+        No.addEventListener('click', () => {
+          deleteConfirmation.style.display = 'none';
+        });
+      }
+    }, 800);
   }
 
+  async getAllPosts() {
+    this.postService.getPosts().subscribe((data: PostSchema[]) => {
+      this.allPosts = data.slice(0, 4);
+    });
+  }
+  async sendDetailstoUpdatePage(details: PostSchema) {
+    this.postService.setter(details);
+    this.route.navigate(['update-post']);
+  }
+
+  async deletePost(id: string, filename: string) {
+    const deleted = await this.postService.deletePost(id);
+    const unlinked = await this.postService.unlinkServerImage(filename);
+    this.getAllPosts();
+    console.log(deleted);
+  }
 }
