f44c967ec806405e01546368f396fb2a8d06a6ab
Muhammad Nouman Munir <noumanmunir458@gmail.com>
2022-11-23 20:48:17 +0500

    Populating single post shifted from slug to id for all posts

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -71,7 +71,7 @@ export class FooterComponent implements OnInit {
   }
   async populateSinglePostData(details: PostSchema) {
     this.postService.setter(details);
-    this.route.navigate([`single-post/${details.slug}`]);
+    this.route.navigate([`single-post/${details._id}`]);
   }
   async deletePost(id: string, filename: string) {
     const deleted = await this.postService.deletePost(id);
--- src/app/shared/components/social-links/social-links.component.ts
+++ src/app/shared/components/social-links/social-links.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-social-links',
   templateUrl: './social-links.component.html',
-  styleUrls: ['./social-links.component.scss']
+  styleUrls: ['./social-links.component.scss'],
 })
 export class SocialLinksComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
